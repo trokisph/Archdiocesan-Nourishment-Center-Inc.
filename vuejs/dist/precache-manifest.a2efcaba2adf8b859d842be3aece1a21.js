4e185a0a74d80d116de1c52ae61901941ed7a6cd
Chester Alan <chesteralantagudin@gmail.com>
2020-06-04 13:13:21 +0800

    Added dist folder

---
+++ vuejs/dist/precache-manifest.a2efcaba2adf8b859d842be3aece1a21.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbbd00b33347d16f8bf",
+    "url": "/css/about.4f606eaa.css"
+  },
+  {
+    "revision": "1573bd94d93a49547434",
+    "url": "/css/app.b0505308.css"
+  },
+  {
+    "revision": "509191bc618b964d3766",
+    "url": "/css/daily-feeding',.4f606eaa.css"
+  },
+  {
+    "revision": "76f581f8fc3749c61b91",
+    "url": "/css/educational-assistance',.4f606eaa.css"
+  },
+  {
+    "revision": "7aaeeeac1139659b34ad",
+    "url": "/css/events.4f606eaa.css"
+  },
+  {
+    "revision": "1dc4437a1a7a06226480",
+    "url": "/css/how-to-help.4f606eaa.css"
+  },
+  {
+    "revision": "596f61f92ab33ccd7536",
+    "url": "/css/livelihood-program',.4f606eaa.css"
+  },
+  {
+    "revision": "0c69a77334fce4f7ff51",
+    "url": "/css/medical-dental',.4f606eaa.css"
+  },
+  {
+    "revision": "9f232d7308d304f6d2fb",
+    "url": "/css/our-centers.4f606eaa.css"
+  },
+  {
+    "revision": "fba48b27c058ceddf8aa",
+    "url": "/css/our-staff.4f606eaa.css"
+  },
+  {
+    "revision": "75aed05fc0627403f141",
+    "url": "/css/parents-collaboration',.4f606eaa.css"
+  },
+  {
+    "revision": "095e3a0dd1622591858c",
+    "url": "/css/programs-and-services',.4f606eaa.css"
+  },
+  {
+    "revision": "86fec956896045961dd5",
+    "url": "/css/spiritual-formation',.4f606eaa.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6d46c16e7fda94ba309b7d79d8140e92",
+    "url": "/img/alchie.6d46c16e.jpg"
+  },
+  {
+    "revision": "d5d7266d4d495d959deda165505f2bc4",
+    "url": "/img/anc-building.d5d7266d.jpg"
+  },
+  {
+    "revision": "e15292bb8669453fd3d065ba2bf90fb0",
+    "url": "/img/anc-concert-2019.e15292bb.jpg"
+  },
+  {
+    "revision": "8ad42ed7a5cbfefb653d80ca20e12610",
+    "url": "/img/apple-touch-icon.8ad42ed7.png"
+  },
+  {
+    "revision": "d3370cdf9c7d61263542c44ca6c4fceb",
+    "url": "/img/ate-marina.d3370cdf.jpg"
+  },
+  {
+    "revision": "b3b2edcd97c6bfee108d5754aefbb0e4",
+    "url": "/img/cd-icon-controller.b3b2edcd.svg"
+  },
+  {
+    "revision": "e8ffcff1b2d29a2d6209835f6254dbde",
+    "url": "/img/cd-top-arrow.e8ffcff1.svg"
+  },
+  {
+    "revision": "ffc45b06cfe5d0bb09baf290678917ef",
+    "url": "/img/daily-feeding.ffc45b06.jpg"
+  },
+  {
+    "revision": "26a010b7b6991959b6e9ff94380c9ba5",
+    "url": "/img/dummy-img-400x400.26a010b7.jpg"
+  },
+  {
+    "revision": "6fd4b554d6b62cf71ba9ea43f472de2c",
+    "url": "/img/educational-assistance.6fd4b554.jpg"
+  },
+  {
+    "revision": "b8c1d6662d56a6a6eb8f197ce2e1f2fa",
+    "url": "/img/feed-a-child-2.b8c1d666.jpg"
+  },
+  {
+    "revision": "4adbfc63d70501bd04f781bbdfc081a0",
+    "url": "/img/feed-a-child-3.4adbfc63.jpg"
+  },
+  {
+    "revision": "15e51068b1488a6e7363415de4214e3f",
+    "url": "/img/feed-a-child.15e51068.jpg"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "166a21f1fa22ae27f3d7ef81585ee6b4",
+    "url": "/img/footer-map.166a21f1.jpg"
+  },
+  {
+    "revision": "cd16786d9a6a9ebe84bac4faa0437d10",
+    "url": "/img/header-1.cd16786d.jpg"
+  },
+  {
+    "revision": "6aa6c96b21c3ad066e6ecf0092817328",
+    "url": "/img/header-2.6aa6c96b.jpg"
+  },
+  {
+    "revision": "24994aaafe95e217131caa17c39cc24b",
+    "url": "/img/header-3.24994aaa.jpg"
+  },
+  {
+    "revision": "852b53485403d45a1dcd90cf36d5f1c2",
+    "url": "/img/header-4.852b5348.jpg"
+  },
+  {
+    "revision": "54ab72bd45d091738b5260f65e9bdcbd",
+    "url": "/img/help1.54ab72bd.png"
+  },
+  {
+    "revision": "62351b80533f6fb044c75a1d89696652",
+    "url": "/img/livelihood-program.62351b80.jpg"
+  },
+  {
+    "revision": "a1e1ee02a3eaeecaf4926b3292f5e79b",
+    "url": "/img/logo.a1e1ee02.png"
+  },
+  {
+    "revision": "a5344789d3595824c2e823bfcf65bf5d",
+    "url": "/img/logo2.a5344789.png"
+  },
+  {
+    "revision": "94eab72c349d27933d331bfeadf45247",
+    "url": "/img/medical-dental.94eab72c.jpg"
+  },
+  {
+    "revision": "3c4e28b3555e95dc5d2884645fa9c064",
+    "url": "/img/msgr-paul.3c4e28b3.jpg"
+  },
+  {
+    "revision": "9e0758d4c3fe4b9db4cdb5a41032038a",
+    "url": "/img/parents-collaboration.9e0758d4.jpg"
+  },
+  {
+    "revision": "c9e1c32f1525daa5d6da0bd044d63e06",
+    "url": "/img/spiritual-formation.c9e1c32f.jpg"
+  },
+  {
+    "revision": "bdb51b2c2d50824c8458480db67cd25c",
+    "url": "/img/sr-mafel.bdb51b2c.jpg"
+  },
+  {
+    "revision": "a7538630eef9771977575a1e8aa74b5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbbbd00b33347d16f8bf",
+    "url": "/js/about.044c4b07.js"
+  },
+  {
+    "revision": "1573bd94d93a49547434",
+    "url": "/js/app.2936c12e.js"
+  },
+  {
+    "revision": "94b19b56bd4db63ab6da",
+    "url": "/js/chunk-vendors.b29e1eb5.js"
+  },
+  {
+    "revision": "509191bc618b964d3766",
+    "url": "/js/daily-feeding',.4d4f9742.js"
+  },
+  {
+    "revision": "76f581f8fc3749c61b91",
+    "url": "/js/educational-assistance',.b5b61f41.js"
+  },
+  {
+    "revision": "7aaeeeac1139659b34ad",
+    "url": "/js/events.81b5dae4.js"
+  },
+  {
+    "revision": "1dc4437a1a7a06226480",
+    "url": "/js/how-to-help.c0416724.js"
+  },
+  {
+    "revision": "596f61f92ab33ccd7536",
+    "url": "/js/livelihood-program',.db1e50d3.js"
+  },
+  {
+    "revision": "0c69a77334fce4f7ff51",
+    "url": "/js/medical-dental',.88e96563.js"
+  },
+  {
+    "revision": "9f232d7308d304f6d2fb",
+    "url": "/js/our-centers.512ba14a.js"
+  },
+  {
+    "revision": "fba48b27c058ceddf8aa",
+    "url": "/js/our-staff.f2ee560d.js"
+  },
+  {
+    "revision": "75aed05fc0627403f141",
+    "url": "/js/parents-collaboration',.94d92e6d.js"
+  },
+  {
+    "revision": "095e3a0dd1622591858c",
+    "url": "/js/programs-and-services',.3997665d.js"
+  },
+  {
+    "revision": "86fec956896045961dd5",
+    "url": "/js/spiritual-formation',.43926d94.js"
+  },
+  {
+    "revision": "ed7986b139f9890ed716c73531e13d4f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
